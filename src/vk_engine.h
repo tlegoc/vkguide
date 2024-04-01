76a1744ef15fdf3d79b240433435db7e80b880ba
Théo Le Goc <theo@legoc.net>
2024-04-01 16:45:15 +0200

    Init

---
+++ src/vk_engine.h
@@ -0,0 +1,42 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _physical_device;
+	VkDevice _device;
+	VkSurfaceKHR _surface;
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+	bool stop_rendering{ false };
+	VkExtent2D _windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+};
