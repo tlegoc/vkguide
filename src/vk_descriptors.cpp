9c4c5090ec3709bdc07404ea5ca6e2cfc3b950c1
Théo Le Goc <theo@legoc.net>
2024-04-24 13:02:57 +0200

    Cont. 3 electric boogaloo

--- src/vk_descriptors.cpp
+++ src/vk_descriptors.cpp
@@ -1,1 +1,35 @@
 ﻿#include <vk_descriptors.h>
+
+void DescriptorLayoutBuilder::add_binding(uint32_t binding, VkDescriptorType type)
+{
+	VkDescriptorSetLayoutBinding newbind {};
+	newbind.binding = binding;
+	newbind.descriptorCount = 1;
+	newbind.descriptorType = type;
+
+	bindings.push_back(newbind);
+}
+
+void DescriptorLayoutBuilder::clear()
+{
+	bindings.clear();
+}
+
+VkDescriptorSetLayout DescriptorLayoutBuilder::build(VkDevice device, VkShaderStageFlags shaderStages)
+{
+	for (auto& b : bindings) {
+		b.stageFlags |= shaderStages;
+	}
+
+	VkDescriptorSetLayoutCreateInfo info = {.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO};
+	info.pNext = nullptr;
+
+	info.pBindings = bindings.data();
+	info.bindingCount = (uint32_t)bindings.size();
+	info.flags = 0;
+
+	VkDescriptorSetLayout set;
+	VK_CHECK(vkCreateDescriptorSetLayout(device, &info, nullptr, &set));
+
+	return set;
+}
